bc5ce77fb9e36c438cda7ecbb4afd9dbd620ea2f
Shubham781 <164032422+Shubham781@users.noreply.github.com>
2024-08-13 08:45:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,25 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-app.use('/arcgis_js_api', express.static(path.join(__dirname, 'public/arcgis_js_api')));
+app.use(express.static('public', {
+  setHeaders: (res, path, stat) => {
+    if (path.endsWith('.js')) {
+      res.set('Content-Type', 'application/javascript');
+    } else if (path.endsWith('.css')) {
+      res.set('Content-Type', 'text/css');
+    }
+  }
+}));
+
+app.use('/arcgis_js_api', express.static(path.join(__dirname, 'public/arcgis_js_api'), {
+  setHeaders: (res, path, stat) => {
+    if (path.endsWith('.js')) {
+      res.set('Content-Type', 'application/javascript');
+    } else if (path.endsWith('.css')) {
+      res.set('Content-Type', 'text/css');
+    }
+  }
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
